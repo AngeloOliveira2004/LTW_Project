72d795de6e9bc14efffcf6a530081a868286f652
Bruno Miguel Ataide Fortes <52974677+Bruno081002@users.noreply.github.com>
2024-04-15 09:46:30 +0100

    doing the homepage

--- pages/homepage.php
+++ pages/homepage.php
@@ -5,15 +5,16 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <meta name="viewport" content="width=device-width, initial-scale = 1.0">
+    <title>Ecommerce</title> 
+    <link rel="stylesheet" href="homepage.css">
 </head>
 <body>
     <header>
         <nav id = 'menu'>
             <input type="checkbox" id='hamburguer'>
             <label class="hamburger" for="hamburger"></label>
-        </nav>
+        </nav>  
         <h1>Bem Vindo a loja</h1>
         <form action="resultado_da_pesquisa" method= "GET">
             <input type="text" name="pesquisa" placeholder="pesquisar">
@@ -24,8 +25,16 @@
                 <li>catalogo</li>
                 <li>Membro</li>
                 <li>Help center</li>
-            </ul>
+            </ul>  
         </nav>
+        <nav>
+            <ul>
+                <li><a href="#">homem</a></li>
+                <li><a href="#">mulher</a></li>
+                <li><a href="#">crianca</a></li>
+                <li><a href="#">Desporto</a></li>
+        </ul>
+    </nav>
         <a href="loginpage.php" id="loginLink"><button id="openLogincard">Login</button></a>
     </header>
 </body>
