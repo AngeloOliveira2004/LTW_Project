7168d4f8e1268c2019f2cc3d328e6c08fbee41f7
JoseCostaUni <up202207871@up.pt>
2024-04-16 19:11:21 +0100

    Finished home page

---
+++ pages/header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <title>Pet Shelter</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- icon library -->
+    <link href="https://fonts.gstatic.com" rel="preconnect">
+    <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <!-- -->
+    
+
+    <link href="../css/header.css" rel="stylesheet">
+    
+  </head>
+
+    <header>
+      <nav class = "navbar">
+        <div class = "navdiv"> 
+            <div class = "logo"> 
+                <a href="homepage.php">
+                    RetroRags
+                </a>
+            </div>
+            <ul>
+                <li>
+                    <a href="wishlist.php"> <img src="assets/heart.png" alt="Wishlist"> </a>
+                </li>
+                <li>
+                    <a href="profile.php"> <img src="assets/user.png" alt="Profile"> </a>
+                </li>
+                <li>
+                    <a href="messages.php"> <img src="assets/messenger.png" alt="Messages"> </a>
+                </li>
+                <li>
+                    <a href="shopping.php"> <img src="assets/shopping-cart.png" alt="Shopping Cart"> </a>
+                </li>
+                <li>
+                    <a href="shopping_cart.php"> <button>Anuncie Agora</button></a>
+                </li>
+            </ul>
+        </div>
+      </nav>
+    </header>
