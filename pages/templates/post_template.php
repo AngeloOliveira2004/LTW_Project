ea947e851467d4590cdcd505dbd1577f5204787b
JoseCostaUni <up202207871@up.pt>
2024-04-17 21:51:13 +0100

    Finished creating item template

--- pages/templates/post_template.php
+++ pages/templates/post_template.php
@@ -1,27 +1,48 @@
-<?php
+THIS CODE WILL ONLY WORK IF THERE IS A DATABASE IN THE DIRECTORY ABOVE THIS ONE
+TO BE RESOLVED IN THE FUTURE
 
+It works as intended though, so I'm happy with it for now
+
+
+<?php
+// Include required files
+
+if (isset($_POST['execute'])) {
+    require_once '../../db_handler/Item.php';
+    require_once '../../db_handler/OrderHistory.php';
+    require_once '../../db_handler/OrderItems.php';
+    require_once '../../db_handler/ShoppingCart.php';
+    require_once '../../db_handler/Users.php';
+    require_once '../../db_handler/Wishlist.php';
     require_once '../../db_handler/DB.php';
 
+    // Your predetermined function
     $db = new Database();
 
     $items = $db->getItems();
-
     $item = $items[0];
+
+/*
+    foreach ($items as $item) {
+        // Do something with each item
+        echo $item->getName() . '<br>';
+    }
+    */
 
     function generate_item_listing($item) {
         // Initialize the listing HTML
         $listing = '<div class="item-listing">';
         
         // Display the item name
-        $listing .= '<h2>' . $item['name'] . '</h2>';
+        $listing .= '<h2>' . $item->getName() . '</h2>';
     
         // Display the item price
-        $listing .= '<p>Price: $' . number_format($item['price'], 2) . '</p>';
+        $listing .= '<p>Price: $' . number_format($item->getPrice(), 2) . '</p>';
     
         // Check if the item has a photo
-        if (!empty($item['photo_img_col'])) {
+        if ($item->getPhoto() !== null){
             // Display the item photo
-            $listing .= '<img src="data:image/jpeg;base64,' . base64_encode($item['photo_img_col']) . '" alt="Item Photo">';
+            $listing .= '<img src="data:image/jpeg;base64,' . base64_encode($item->getPhoto()) . '" alt="Item Photo">';
         } else {
             // If no photo is available, display a placeholder
             $listing .= '<div class="placeholder-photo">No Photo Available</div>';
@@ -37,5 +58,7 @@
 
 
 echo $item_listing;
+}
+
 
 ?>
