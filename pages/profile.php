ca588e71d733938a7aaf1c66f7362106bb04460a
AngeloOliveira2004 <angeloaraujo2004@gmail.com>
2024-04-22 13:38:49 +0100

    Added profile php and css

--- pages/profile.php
+++ pages/profile.php
@@ -3,29 +3,40 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Profile page</title>
+    <link rel="stylesheet" href="../css/profile.css">
 </head>
 <body>
-    <header>
-        <div class="logo">
-            <img src="assets/logo.png" alt="">
-        </div>
-        <div class="search-bar">
-            <img src="assets/search.png" alt="">
-        </div>
-        <div class="logos">
-            <img src="assets/heart.png" alt="">
-        </div>
-        <div class="logos">
-            <img src="assets/heart.png" alt="">
-        </div>
-        <div class="logos">
-            <img src="assets/shopping-cart.png" alt="">
-        </div>
-        <div class="logos">
-            <img src="assets/user.png" alt="">
-        </div>
-        <button>Button</button>
-    </header>
+    <section class="profile_info">
+            <img src="assets/profile/keanu.jpeg" alt="Keanu Reeves">
+
+        <div class="user_details">
+            <ul>
+                <li>keanu_reeves_007</li>
+                <li>4.9 stars</li>
+                <li>20 reviews</li>
+            </ul>  
+        
+            <ul>
+                <li>About:</li>
+                <li><img src="assets/profile/location.png" alt="location icon">                                                                                                                     Sacramento, United States</li>
+                <li><img src="assets/profile/clock.png" alt="clock icon"> Last online: 1 day ago</li>
+                <li><img src="assets/profile/product.png" alt="product icon"> Items sold: 5</li>
+            </ul>
+        </div>  
+        <button class="edit_profile"><img src="assets/profile/editpencil.png" alt="pencil"> Edit Profile</button>
+    </section>
+
+    <section class="profile_option">
+        <ul>
+            <li>
+                <a href="">Listings</a>
+            </li>
+            <li>
+                <a href="">Ratings</a>
+            </li>
+        </ul>
+    </section>
+    <div class="blue-line"></div>
 </body>
 </html>
