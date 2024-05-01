4cac0d963b973204bfc136cf1bc6e93a6365d70a
AngeloOliveira2004 <angeloaraujo2004@gmail.com>
2024-05-01 13:03:45 +0100

    Added to messages

--- pages/messages.php
+++ pages/messages.php
@@ -3,9 +3,9 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Profile page</title>
+        <title>Messages</title>
         <link href="https://fonts.googleapis.com/css2?family=Crimson+Pro:ital,wght@0,200..900;1,200..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="../css/profile.css">
+        <link href="../css/messages.css" rel="stylesheet">
     </head>
     <body>
         <?php
@@ -19,8 +19,9 @@
             ?>
         <header>
 
+    <section class="messages-section">
         <article class="messages-rectangle">
-            <h2>Messages Rectangle</h2>
+            <h2>Users</h2>
             <?php 
             $db = new Database();
             $userId = $_SESSION['userId'];
@@ -29,20 +30,19 @@
             
             foreach ($messages as $message) {
                 $sender = $message->getSender();
-                echo '<div>';
-                echo '<h3>From: ' . $sender->getUsername() . '</h3>';
+                echo '<div class="user-box">';
+                echo '<h3><img src="../assets/users/' . $sender->getId() . '.png" alt="' . $sender->getUsername() . '" id="item_image">' . $sender->getUsername() . '</h3>';
                 echo '<p>Content: ' . $message->getContent() . '</p>';
                 echo '</div>';
             }
             ?>
 
         </article>
-    </section>
 
-        <article class="messages-rectangle">
-            <h2>Messages Rectangle</h2>
+        <article class="open-messages">
+            <h2>Messages Hub</h2>
         </article>
-        </section>
+    </section>
         
         <footer>
             <?php
