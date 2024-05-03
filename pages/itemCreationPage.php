b7a46ddcc06b2d9ae6633564528869fcbb6b553c
JoseCostaUni <up202207871@up.pt>
2024-05-03 11:05:43 +0100

    Styling Create Item Page

--- pages/itemCreationPage.php
+++ pages/itemCreationPage.php
@@ -4,30 +4,42 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ItemCreationPage</title>
+
+    <link href="../../css/itemCreation.css" rel="stylesheet">
+    
 </head>
 
 <body>
         <?php
             include 'templates/header.php';
         ?>
+
+        <p class = "publish">Publicar Anúncio</p>
         <div class = "ItemCreation">
-        <p>Publicar Anúncio</p>
 
         <section class = "Item-Title-And-Categories">
-            <p>Introduza um ótimo título!*</p>
-            
+            <p>Primeiro , introduz um título*</p>
+
+            <p class = "box_title">Tenta ser o mais descritivo e apelativo pssível</p>
+            <div class = >
+
+            </div>
             <input type="text" class = "Item-Title">
 
             <p>Categoria</p>
 
             <ul class = "Categories">
-
+                <li>
+                    <input type="text">
+                </li>
             </ul>
 
             <p>Sub-Categoria</p>
 
             <ul class = "Sub-Categories">
-
+                <li>
+                    <input type="text">
+                </li>
             </ul>
         </section>
 
@@ -50,8 +62,6 @@
 
         <section class = "Item-Price-Trade">
             <button class = "Preço">Preço</button>
-            <button class = "Troca">Troca</button>
-            <button class = "Ambos">Ambos</button>
 
             <input type="text" class ="Preço">
 
