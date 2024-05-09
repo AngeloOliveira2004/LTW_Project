e64a40cdf2c6ba4f7a16c7853ae8ea0ad7324418
AngeloOliveira2004 <angeloaraujo2004@gmail.com>
2024-05-09 17:47:29 +0100

    Solved issue with image just changing 1 time in edit profile

--- pages/js/editprofile.js
+++ pages/js/editprofile.js
@@ -1,21 +1,35 @@
 document.addEventListener('DOMContentLoaded', function() {
     const reveal_button = document.getElementById("profile-picture");
     reveal_button.addEventListener("change", previewAndUploadImage);
+
+    const clearButton = document.getElementById("profile-picture");
+    clearButton.addEventListener("click", clearInputFile);
 });
 
 function previewAndUploadImage(event) {
-    var file = event.target.files[0];
-    var formData = new FormData();
+    let file = event.target.files[0];
+    let formData = new FormData();
     formData.append('profile_image', file);
 
-    var xhr = new XMLHttpRequest();
+    let xhr = new XMLHttpRequest();
     xhr.open('POST', '../../db_handler/upload_image.php');
     xhr.onload = function() {
         if (xhr.status >= 200 && xhr.status < 300) {
-            document.getElementById('profile-image').src = xhr.responseText;
+            let imageUrl = xhr.responseText + '?' + Math.random();
+            document.getElementById('profile-image').src = imageUrl;
         } else {
             console.error(xhr.responseText);
         }
     };
     xhr.send(formData);
+}
+
+function clearInputFile(event) {
+    const inputFile = document.getElementById("profile-picture");
+
+    if (inputFile.files.length > 0) {
+        inputFile.value = '';
+    }
+
+    event.target.files[0] = '';
 }
