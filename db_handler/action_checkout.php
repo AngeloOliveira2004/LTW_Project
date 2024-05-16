e2f4edf13a462aa3447d7c87ef5030dc8edf084c
AngeloOliveira2004 <angeloaraujo2004@gmail.com>
2024-05-16 18:20:30 +0100

    Added checkout logic

--- db_handler/action_checkout.php
+++ db_handler/action_checkout.php
@@ -3,6 +3,8 @@
 require_once 'DB.php';
 require_once 'Item.php';
 
+session_start();
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $paymentMethod = $_POST["paymentMethod"];
     $shippingMethod = $_POST["shippingMethod"];
@@ -14,6 +16,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $db = new Database("../database/database.db");
 
+    $db->AddOrderHistory($user, $paymentMethod, $shippingMethod, $totalPrice);
+
+    $db->RetrieveLastOrderHistory();
 
 } else {
     // Handle the case where the request method is not POST
--- pages/js/shopping_cart.js
+++ pages/js/shopping_cart.js
@@ -67,7 +67,6 @@ document.addEventListener('DOMContentLoaded', function () {
     
         const xhr = new XMLHttpRequest();
         xhr.open('POST', '../../db_handler/action_checkout.php', true);
-        xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
 
         xhr.onload = function () {
             if (xhr.status >= 200 && xhr.status < 300) {
